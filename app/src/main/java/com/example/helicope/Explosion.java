e78fb61cc2767ce5908aa7f0ae01a2c3ac18e4bb
Ankita Pabbi <erankitapabbi@gmail.com>
2019-06-23 14:09:02 -0400

    Created a java class Explosion to show explosion effect when player collide with missile

---
+++ app/src/main/java/com/example/helicope/Explosion.java
@@ -0,0 +1,28 @@
+package com.example.helicope;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+
+public class Explosion {
+    private int x;
+    private int y;
+    private int row;
+    private Animation animation = new Animation();
+
+    public Explosion(Bitmap res, int x, int y, int width, int height, int numFrames) {
+
+    }
+
+    public void update() {
+        if (!animation.isPlayedOnce()) {
+
+        }
+    }
+
+    public void draw(Canvas canvas) {
+        if ( !animation.isPlayedOnce()){
+            
+        }
+    }
+
+}
