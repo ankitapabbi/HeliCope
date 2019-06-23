6dced2ccbb114e83e692c6f9851cb9140388f1aa
Ankita Pabbi <erankitapabbi@gmail.com>
2019-06-22 23:31:33 -0400

    Created a basic SplashScreen Activity and java class named Game

---
+++ app/src/main/java/com/example/helicope/SplashScreen.java
@@ -0,0 +1,24 @@
+package com.example.helicope;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(getApplicationContext(),Game.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        },5000);
+    }
+}
