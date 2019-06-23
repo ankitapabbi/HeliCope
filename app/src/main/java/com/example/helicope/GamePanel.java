a2a5fc2d35802ddd95babf2c2b6a0ed064b610bb
Ankita Pabbi <erankitapabbi@gmail.com>
2019-06-23 14:23:10 -0400

    Creating required variables in GamePanel

--- app/src/main/java/com/example/helicope/GamePanel.java
+++ app/src/main/java/com/example/helicope/GamePanel.java
@@ -4,12 +4,32 @@ import android.content.Context;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+import java.util.ArrayList;
+
 public class GamePanel extends SurfaceView implements SurfaceHolder.Callback{
 
 
     public static final int WIDTH = 856;
     public static final int HEIGHT = 480;
     public static final int MOVESPEED = -5;
+
+    private MainThread thread;
+    private Background bg;
+    private Player player;
+    private ArrayList<SmokePuff> smokePuffs;
+    private ArrayList<Missile> missiles;
+    private ArrayList<TopBorder> topBorders;
+    private ArrayList<BotBorder> botBorders;
+
+    private int progressDenominator = 20;
+
+    private Explosion explosion;
+    private long startReset;
+    private boolean reset;
+    private boolean disappear;
+    private boolean started;
+    private int bestScore;
+    int count = 5;
 
 
     public GamePanel(Context context, int best) {
