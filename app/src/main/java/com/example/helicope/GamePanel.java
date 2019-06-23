3e177e0a6f942cc718e6f8dc1e589f857e5e8efb
Ankita Pabbi <erankitapabbi@gmail.com>
2019-06-23 15:00:37 -0400

    Added functionality to update function to update after some intervals

--- app/src/main/java/com/example/helicope/GamePanel.java
+++ app/src/main/java/com/example/helicope/GamePanel.java
@@ -149,7 +149,124 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback{
 
     }
     public void update(){
+        if (player.isPlaying()) {
 
+            if (botBorders.isEmpty()) {
+                player.setPlaying(false);
+                return;
+            }
+            if (topBorders.isEmpty()) {
+                player.setPlaying(false);
+                return;
+            }
+
+            bg.update();
+            player.update();
+
+            //calculate the threshold of height the border can have based on the score
+            //max and min border heart are updated , and the border switched direction when either max or
+            //min is met
+
+            maxBorderHeight = 30 + player.getScore() / progressDenominator;
+            //cap max border height so that borders can only take up a total of 1/2 the screen
+            if (maxBorderHeight > HEIGHT / 4)
+                maxBorderHeight = HEIGHT / 4;
+            minBorderHeight = 5 + player.getScore() / progressDenominator;
+
+            //check bottom border collision
+            for (int i = 0; i < botBorders.size(); i++) {
+                if (collision(botBorders.get(i), player)) {
+                    player.setPlaying(false);
+                    break;
+                }
+            }
+
+            //check top border collision
+            for (int i = 0; i < topBorders.size(); i++) {
+                if (collision(topBorders.get(i), player)) {
+                    player.setPlaying(false);
+                    break;
+                }
+            }
+
+            //update top border
+            this.updateTopBorder();
+
+            //update bottom border
+            this.updateBottomBorder();
+
+            //add missiles on timer
+            long missileElapsed = (System.nanoTime() - missileStartTime) / 1000000;
+            if (missileElapsed > (2000 - player.getScore() / 4)) {
+                //first missile always goes down the middle
+                if (missiles.size() == 0) {
+                    missiles.add(new Missile(BitmapFactory.decodeResource(getResources(),
+                            R.drawable.missile), WIDTH + 10, HEIGHT / 2, 45, 15, player.getScore(), 13));
+                } else {
+                    missiles.add(new Missile(BitmapFactory.decodeResource(getResources(),
+                            R.drawable.missile), WIDTH + 10, (int) (rand.nextDouble() * (HEIGHT - (maxBorderHeight * 2)) + maxBorderHeight),
+                            45, 15, player.getScore(), 13));
+                }
+                //reset timer
+                missileStartTime = System.nanoTime();
+            }
+            //loop though every missile and check collision and remove
+            for (int i = 0; i < missiles.size(); i++) {
+                //update missile
+                missiles.get(i).update();
+
+                if (collision(missiles.get(i), player)) {
+
+                    if(count<=5 && count!=0){
+                        missiles.remove(i);
+                        player.setPlaying(true);
+                        count--;
+                    }
+                    else if(count == 0) {
+                        player.setPlaying(false);
+                        count =5;
+                        break;
+                    }
+
+                }
+                //remove missile if it is way off the screen
+                if (missiles.get(i).getX() < -100) {
+                    missiles.remove(i);
+                    break;
+
+                }
+            }
+
+            //add smoke puffs on timer
+            long elapsed = (System.nanoTime() - smokeStartTime) / 1000000;
+            if (elapsed > 120) {
+                smokePuffs.add(new SmokePuff(player.getX(), player.getY() + 10));
+                smokeStartTime = System.nanoTime();
+            }
+
+            for (int i = 0; i < smokePuffs.size(); i++) {
+                smokePuffs.get(i).update();
+                if (smokePuffs.get(i).getX() < -10) {
+                    smokePuffs.remove(i);
+                }
+            }
+        } else {
+            player.resetDY();
+            if (!reset) {
+                newGameCreated = false;
+                startReset = System.nanoTime();
+                reset = true;
+                disappear = true;
+                explosion = new Explosion(BitmapFactory.decodeResource(getResources(), R.drawable.explosion),
+                        player.getX(), player.getY() - 30, 100, 100, 25);
+            }
+            explosion.update();
+            long resetElapsed = (System.nanoTime() - startReset) / 1000000;
+            if (resetElapsed > 2500 && !newGameCreated) {
+                newGame();
+            }
+
+        }
     }
 
     private boolean collision(GameObject a, GameObject b) {
